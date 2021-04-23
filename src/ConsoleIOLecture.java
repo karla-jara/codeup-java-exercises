ee473f3448a91314c6ffda168d383f32ec4edc9e
Karla Jara <jara.karla.43@gmail.com>
2021-04-23 15:55:36 -0500

    added lecture file for console IO

---
+++ src/ConsoleIOLecture.java
@@ -0,0 +1,23 @@
+public class ConsoleIOLecture {
+    public static void main(String[] args){
+        System.out.println("Console IO Lecture");
+
+
+        //=================print() and println()
+
+        //this:
+//        System.out.println("here");
+//        System.out.println("there");
+        // prints here next line there
+
+        System.out.print("here\n");
+        System.out.print("there\n");
+
+
+
+
+
+
+
+    }
+}
