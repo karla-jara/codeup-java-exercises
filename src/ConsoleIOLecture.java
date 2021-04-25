c320d401e0f079b1ce390e9270ae9566e36390f9
Karla Jara <jara.karla.43@gmail.com>
2021-04-24 23:26:10 -0500

    Lecture file

--- src/ConsoleIOLecture.java
+++ src/ConsoleIOLecture.java
@@ -13,6 +13,31 @@ public class ConsoleIOLecture {
         System.out.print("here\n");
         System.out.print("there\n");
 
+        System.out.print("here");
+        System.out.print("there");
+
+        String firstName = "Karla";
+        String lastName = "Jara";
+        System.out.println(firstName + " " + lastName);
+
+
+        /// printf() / .format()
+
+        // Print a formatted string using th following .. printf(formatString)
+
+        // same output as print():
+        System.out.printf("Hello , %s!\n", firstName);
+        System.out.printf("Hello , %s %s!\n", firstName, lastName);
+
+        //numbers
+        int numberOfPets = 3;
+        String typeOfPets = "cats";
+        System.out.printf("I have %d %s. \n", numberOfPets, typeOfPets);
+
+
+        // currency:
+        int currencyPennies = 1000;
+        System.out.printf("My iPhone cost %d", currencyPennies);
 
 
 
