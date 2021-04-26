ccfdd8d0a6df66c6bc588adf3fc47267627f2fd7
Karla Jara <jara.karla.43@gmail.com>
2021-04-26 09:44:15 -0500

    Added files for exercise. Completed first exercise problem

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,11 @@
+
+public class ConsoleExercises {
+
+    public static void main(String [] args){
+
+        //1. copied code to main method:
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f%n", pi);
+
+    }
+}
