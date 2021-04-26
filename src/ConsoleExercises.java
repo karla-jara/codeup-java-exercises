40907e3eb1d47425cf8a1045f93bc9407e17ad46
Karla Jara <jara.karla.43@gmail.com>
2021-04-26 10:16:14 -0500

    working on second problem

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class ConsoleExercises {
 
@@ -6,6 +7,20 @@ public class ConsoleExercises {
         //1. copied code to main method:
         double pi = 3.14159;
         System.out.format("The value of pi is approximately %.2f%n", pi);
+
+        // 2. Prompt a user to enter an integer and store that value in an int variable using nextInt method:
+        // What happens if data entered is not an integer?
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int userInput = in.nextInt();
+        System.out.println("You entered: \"" + userInput + "\" ");
+
+        Scanner words = new Scanner(System.in);
+
+        System.out.print("Please enter 3 words: ");
+
+
 
     }
 }
