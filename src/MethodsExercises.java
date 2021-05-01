56a6171036ae040dd4d70bd480a29e719a28b9f7
Karla Jara <jara.karla.43@gmail.com>
2021-05-01 03:08:39 -0500

    Exercise problem 3 updated

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -14,21 +14,64 @@ public class MethodsExercises {
 
         //2.
         getInteger(1,10);
+        //3.
+        getFactorial(3);
     }
 
     // 2. Method that validates user input is in certain range
-    public static int getInteger (int min, int max) {
+    public static int getInteger (int min, int max){
         Scanner in = new Scanner(System.in);
         System.out.print("Please enter a number between 1 and 10: ");
         int inputNum = in.nextInt();
-        if (inputNum > 0 && inputNum <= 10) {
+        if (inputNum > 0 && inputNum <= 10){
             System.out.println("Congratulations, your number is valid!");
-        } else {
+        } else{
             System.out.println("Sorry, that is not within the range of 1 to 10");
             getInteger(1, 10);
         }
         return inputNum;
     }
+
+    //3. Finds the factorial value
+    public static void goAgain(){
+        System.out.println("Ok, thank you for playing. Come back any time!");
+    }
+
+    public static long getFactorial (int num){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a number between 1 and 10: ");
+        long inputNum = in.nextLong();
+        if (inputNum > 0 && inputNum <= 10){
+            Scanner proceed = new Scanner(System.in);
+            System.out.println("Number " + inputNum + " is valid. Would you like to continue? \n Please enter Y for yes or N for no.");
+            String answer = proceed.next();
+            if (answer.equalsIgnoreCase("y")){
+                long factored = 1;
+                for (var f = 1; f <= inputNum; f++){
+                    factored = factored * f;
+                    System.out.println(inputNum + "! = " + inputNum + " x " + f + " = " + factored);
+                }
+            }else if (answer.equalsIgnoreCase("n")){
+                System.out.println("Ok, thank you for playing. Come back any time!");
+            }
+
+                Scanner proceed2 = new Scanner(System.in);
+                System.out.println("Would you like to continue? \n Please enter Y for yes or N for no.");
+                String answer2 = proceed2.next();
+
+                if (answer.equalsIgnoreCase("n")){
+                    System.out.println("Ok, thank you for playing. Come back any time!");
+                }else if (answer.equalsIgnoreCase("y")){
+                    getFactorial(num);
+                } else {
+                    System.out.println("Sorry, you entry was not valid :(");
+                }
+            System.out.println("Sorry, you entry was not valid :(");
+        }
+        return inputNum;
+    }
+
+
 
         /*
     public static long getPower(int base, int exponent) {
