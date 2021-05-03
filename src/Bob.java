04ecad6f6408fdd86dbb6c3747a452fa2dbe7041
Karla Jara <jara.karla.43@gmail.com>
2021-05-03 00:49:42 -0500

    Added code for Bob exercise problem

--- src/Bob.java
+++ src/Bob.java
@@ -3,14 +3,32 @@ public class Bob {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Do you want to talk with Bob?");
-        System.out.println("Gotta warn you... Bob is a teenager");
-        System.out.println("Enter Y to proceed, or N is you changed your mind");
+        System.out.print("Do you want to talk with Bob?\n");
+        System.out.println("Gotta warn you... Bob is a teenager\n");
+        System.out.println("Enter Y to proceed, or N if you changed your mind: \n");
 
 
         String wanaTalk = in.next();
-        if (wanaTalk.equalsIgnoreCase("Y")){
 
+        while (wanaTalk.equalsIgnoreCase("Y")){
+            System.out.println("Go ahead..");
+            Scanner in2 = new Scanner(System.in);
+            String talking = in2.next();
+            if (talking.endsWith("?")){
+                System.out.println("Sure.");
+            }
+            if (talking.endsWith("!")){
+                System.out.println("Whoa, chill out!");
+            }
+            if (talking.endsWith(" ")){
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+        }
+        while (wanaTalk.equalsIgnoreCase("N")){
+            System.out.println("That's ok.. we get it");
+            break;
         }
     }
 
