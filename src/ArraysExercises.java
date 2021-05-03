9b9c16613a3185bdec8e14535ef731d5efc2c127
Karla Jara <jara.karla.43@gmail.com>
2021-05-03 16:54:42 -0500

    added notes for lecture on Arrays, as well started array exercise. Almost done with first problem

---
+++ src/ArraysExercises.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args){
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+//        String [] person = {"person1", "person2", "person3"};
+            // both code top and bottom create and set array index
+        String [] person =  new String[3];
+        person[0] = "person1";
+        person[1] = "person2";
+        person[2] = "person3";
+        for(String eachPeep : person){
+            System.out.println("eachPeep = " + eachPeep);
+        }
+
+    }
+    public static String addPerson(String person){
+        String [] newPersonList = Arrays.copyOf(new String[]{person}, 4);
+
+        System.out.println(Arrays.toString(newPersonList));
+//    }
+
+}
--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,7 @@
-public class Person {
-   private String name;
+public class Person { // defines a custom type
+   private String name; // this is a variable property
 
-    public String getName(){
+    public String getName(){ // this is a method function
 //TODO: return the person's name
         return name;
     }
@@ -9,6 +9,7 @@ public class Person {
    public void setName (String name){
        // change the name property to the passed value
        this.name = name;
+       System.out.println("Last name is: " + name);
    }
 
    public void sayHello(){
@@ -20,10 +21,33 @@ public class Person {
         this.name = name;
    }
 
-   public static void main(String[] args){
-       Person one = new Person("Karla" );
-       public static String person(){
+   public static void main(String[] args) {
+       Person name = new Person("Karla"); //this references ln 2
+       System.out.println(name.getName()); // calling ln 4 for retrieving created name on ln 25
+       name.setName("Jara");
+       name.sayHello();
 
-       }
-   }
+
+   // 1. Understanding references: guessed the return to be two true statements
+//       Person person1 = new Person("John");
+//       Person person2 = new Person("John");
+//       System.out.println(person1.getName().equals(person2.getName()));
+//       System.out.println(person1 == person2); // this is calling the value
+
+       // predict true
+//       Person person1 = new Person("John");
+//       Person person2 = person1;
+//       System.out.println(person1 == person2);
+
+       // predict John, John, Jane, John, Jane
+       Person person1 = new Person("John");
+       Person person2 = person1;
+       System.out.println(person1.getName());
+       System.out.println(person2.getName());
+       person2.setName("Jane");
+       System.out.println(person1.getName());
+       System.out.println(person2.getName());
+    }
+
+
 }
