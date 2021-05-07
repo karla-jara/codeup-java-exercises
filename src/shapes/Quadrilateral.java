66d014a38ed9648ecfd4ae5c76e7aa2ed4e2bb71
Karla Jara <jara.karla.43@gmail.com>
2021-05-07 09:08:42 -0500

    added code for abstract exercise

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,5 +1,24 @@
 package shapes;
 
-public class Quadrilateral extends Shape {
+abstract class Quadrilateral extends Shape {
+
+    protected int length;
+    protected int width;
+
+    public Quadrilateral(int l, int w){
+            length = l;
+            width = w;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+
+
 
 }
